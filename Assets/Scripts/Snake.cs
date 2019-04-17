36f26cd592b5cee4b6b08d09e9b530e61665d4cc
slevy2020 <slevy@chadwickschool.org>
2019-04-16 17:57:41 -0700

    feat: add basic snake game

---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Snake : MonoBehaviour {
+    Vector2 currentDir = Vector2.right;
+
+    bool ate = false;
+    public GameObject tailPrefab;
+
+    List<Transform> tail = new List<Transform>();
+
+    void Start() {
+      InvokeRepeating("Move", 0.3f, 0.05f);
+    }
+
+    void Update() {
+      if (Input.GetKeyDown("w")) {
+        currentDir = Vector2.up;
+      } else if (Input.GetKeyDown("s")) {
+        currentDir = -Vector2.up;
+      } else if (Input.GetKeyDown("d")) {
+        currentDir = Vector2.right;
+      } else if (Input.GetKeyDown("a")) {
+        currentDir = -Vector2.right;
+      }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+      if (collision.tag == "food") {
+        ate = true;
+        Destroy(collision.gameObject);
+      } else {
+        //wall or snake, go to game end screen
+      }
+    }
+
+    void Move() {
+      Vector2 gapPos = transform.position;
+      transform.Translate(currentDir/2);
+
+      if (ate) {
+        for (int i=0; i<2; i++) {
+          GameObject tailObj = Instantiate(tailPrefab, gapPos, Quaternion.identity);
+          tail.Insert(0, tailObj.transform);
+        }
+        ate = false;
+      }
+      else if (tail.Count > 0) {
+        tail.Last().position = gapPos;
+        tail.Insert(0, tail.Last());
+        tail.RemoveAt(tail.Count-1);
+      }
+    }
+}
---
+++ Assets/Scripts/SpawnFood.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFood : MonoBehaviour {
+  public GameObject foodPrefab;
+
+  public Transform borderTop;
+  public Transform borderBottom;
+  public Transform borderLeft;
+  public Transform borderRight;
+
+  void Start() {
+    InvokeRepeating("Spawn", 3f, 4f);
+  }
+
+  public void Spawn() {
+    float x = Random.Range(borderLeft.position.x + .5f, borderRight.position.x - .5f);
+    float y = Random.Range(borderBottom.position.y + .5f, borderTop.position.y - .5f);
+
+    Instantiate(foodPrefab, new Vector2(x, y), Quaternion.identity);
+  }
+}

85e40cb4e25b8a7384f3cf850f6692ba3a8427da
slevy2020 <slevy@chadwickschool.org>
2019-04-17 20:00:20 -0700

    feat: add start screen
    i also added some scenes/assets in prep for the other two scenes

---
+++ Assets/Scripts/Play.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Play : MonoBehaviour {
+  void Start(){
+    Button start = GetComponent<Button>();
+    start.onClick.AddListener(StartGame);
+  }
+  void StartGame() {
+    SceneManager.LoadScene("Level");
+  }
+}
---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Quit : MonoBehaviour {
+  void Start(){
+    Button quit = GetComponent<Button>();
+    quit.onClick.AddListener(QuitGame);
+  }
+
+  void QuitGame() {
+    Debug.Log("quit");
+    Application.Quit();
+  }
+}

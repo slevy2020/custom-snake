6a9cbe3b0ac19875f716f7d11cec7e87e47a72a6
slevy2020 <slevy@chadwickschool.org>
2019-04-22 12:48:50 -0700

    saving work on test shop item

---
+++ Assets/Scripts/BuyFromStore.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyFromStore : MonoBehaviour {
+  private PersistentData persistentScript;
+
+  void Start() {
+    persistentScript = GameObject.Find("Persistent Object").GetComponent<PersistentData>();
+  }
+}

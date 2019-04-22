52ab194cc5eb7bb577590220f58509f53047d179
slevy2020 <slevy@chadwickschool.org>
2019-04-22 09:11:18 -0700

    feat: add money display to the shop

---
+++ Assets/Scripts/MoneyDisplay.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyDisplay : MonoBehaviour {
+
+  private PersistentData persistentScript;
+  public Text moneyDisplayText;
+
+  void Start() {
+    persistentScript = GameObject.Find("Persistent Object").GetComponent<PersistentData>();
+  }
+
+  void Update() {
+    moneyDisplayText.text = "You have $" + persistentScript.money;
+  }
+}

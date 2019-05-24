1851580eadfde425253acc5224d7c12046d12b13
slevy2020 <slevy@chadwickschool.org>
2019-05-24 09:45:42 -0700

    feat: add tiered upgrades
    it works! well it's more of a proof of concept for now, i just need to add the applications for it.

--- Assets/Scripts/BuyFromStore.cs
+++ Assets/Scripts/BuyFromStore.cs
@@ -47,7 +47,7 @@ public class BuyFromStore : MonoBehaviour {
       //if the player can afford it, set bought to true
       if (cost <= persistentScript.money) {
   //      currentUpgradeTier += 1;
-        if (upgradeTier.Length != persistentScript.MAX_UPGRADES[storeIndex]) {
+        if (persistentScript.upgradeTiers[storeIndex] >= (persistentScript.MAX_UPGRADES[storeIndex] - 1)) {
           bought = true;
         }
       }
